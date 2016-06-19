9c26abf8c2879ed209edb2d049242b2bf8297b5e
revdaalex <revdaalex@gmail.com>
2016-06-19 15:10:45 +0500

    update CalculatorMenu

--- oodsrp/src/main/java/ru/revdaalex/oodsrp/CalculatorMenu.java
+++ oodsrp/src/main/java/ru/revdaalex/oodsrp/CalculatorMenu.java
@@ -1,24 +1,49 @@
 package ru.revdaalex.oodsrp;
 
+import ru.revdaalex.oodsrp.interfaces.IO;
+
 /**
  * CalculatorMenu contains all user menus.
  * Created by revdaalex on 08.06.2016.
  */
 public class CalculatorMenu {
+    private static final String SELECT = "Выберите действие";
+    /**
+     * Base operations.
+     */
+    private static final String BASE_OPTS = new StringBuilder()
+            .append("1. Сложить\n2. Вычесть\n")
+            .append("3. Умножить\n4. Делить")
+            .toString();
+    /**
+    * Additional operations.
+    */
+    private static final String ADD__OPTS = new StringBuilder()
+            .append("5. Произвести вычисление с полученным результатом\n")
+            .append("6. Очистить\n")
+            .append("7. Выйти из калькулятора")
+            .toString();
+
+    private final IO io;
+
+    public CalculatorMenu(IO io) {
+        this.io = io;
+    }
+
 
     /**
      * Show main menu method.
      */
     public void showMenu(){
-        System.out.println("Выберите действие");
-        System.out.println("1. Сложить\n2. Вычесть\n3. Умножить\n4. Делить\n5. Произвести вычисление с полученным результатом\n6. Очистить\n7. Выйти из калькулятора");
+        this.io.println(SELECT);
+        this.io.println(BASE_OPTS);
+        this.io.println(ADD__OPTS);
     }
 
     /**
      * Show CalcResultMenu method.
      */
     public void showCalcResultMenu(){
-        System.out.println("Выберете оператор");
-        System.out.println("1. Сложить\n2. Вычесть\n3. Умножить\n4. Делить");
+        this.io.println(BASE_OPTS);
     }
 }
