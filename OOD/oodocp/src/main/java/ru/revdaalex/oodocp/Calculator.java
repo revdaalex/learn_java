73d62d229210285f29fee40b06a74da3cab3bd97
revdaalex <revdaalex@gmail.com>
2016-06-22 13:19:23 +0500

    add Calculator.java

---
+++ OOD/oodocp/src/main/java/ru/revdaalex/oodocp/Calculator.java
@@ -0,0 +1,95 @@
+package ru.revdaalex.oodocp;
+
+/**
+ * Calculator logic.
+ * Created by revdaalex on 07.06.2016.
+ */
+public class Calculator {
+
+    /**
+     * final result.
+     */
+    private double result;
+
+    /**
+     * Summation of numbers.
+     * @param first int first.
+     * @param second int second.
+     * @return result.
+     */
+    public double add(double first, double second){
+        return result = first + second;
+    }
+
+    /**
+     * Subtraction of numbers.
+     * @param first int first.
+     * @param second int second.
+     * @return result.
+     */
+    public double sub(double first, double second){
+        return result = first - second;
+    }
+
+    /**
+     * Multiplication.
+     * @param first int first.
+     * @param second int second.
+     * @return result.
+     */
+    public double mult(double first, double second){
+        return result = first * second;
+    }
+
+    /**
+     * Divide numbers.
+     * @param first int first.
+     * @param second int second.
+     * @return result.
+     */
+    public double div(double first, double second){
+        return result = first / second;
+    }
+
+    /**
+     * Clear results.
+     */
+    public void clear(){
+        this.result = 0;
+    }
+
+    /**
+     * Sinus.
+     * @param value degree value.
+     * @return result.
+     */
+    public double sinus(double value){
+        return result = Math.sin(value);
+    }
+
+    /**
+     * Cosine.
+     * @param value degree value.
+     * @return result.
+     */
+    public double cosine(double value){
+        return result = Math.cos(value);
+    }
+
+    /**
+     * Tangent.
+     * @param value degree value.
+     * @return result.
+     */
+    public double tangent(double value){
+        return result = Math.tan(value);
+    }
+
+    /**
+     * Return the result.
+     * @return result.
+     */
+    public double getResult() {
+        return result;
+    }
+}
