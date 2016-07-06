a129a24a018757592385836c81f16f7fb22f68e3
revdaalex <revdaalex@gmail.com>
2016-07-06 16:37:27 +0500

    add Controll.java

---
+++ LSP/ChapterI/src/main/java/ru/revdaalex/lsp/chapterI/interfaces/Controll.java
@@ -0,0 +1,16 @@
+package ru.revdaalex.lsp.chapterI.interfaces;
+
+import ru.revdaalex.lsp.chapterI.food.Food;
+
+/**
+ * Controll interface.
+ * Created by revdaalex on 04.07.2016.
+ */
+public interface Controll {
+    /**
+     * Method sort.
+     * @param food
+     */
+    void sort(Food food);
+
+}
