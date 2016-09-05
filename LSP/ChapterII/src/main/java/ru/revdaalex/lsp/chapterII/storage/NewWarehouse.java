b08de01b8bc45f8c96116dd733523f74b54daba9
revdaalex <revdaalex@gmail.com>
2016-09-05 16:39:44 +0500

    modified NewWarehouse.java

--- LSP/ChapterII/src/main/java/ru/revdaalex/lsp/chapterII/storage/NewWarehouse.java
+++ LSP/ChapterII/src/main/java/ru/revdaalex/lsp/chapterII/storage/NewWarehouse.java
@@ -3,6 +3,8 @@ package ru.revdaalex.lsp.chapterII.storage;
 import ru.revdaalex.lsp.chapterI.food.Food;
 import ru.revdaalex.lsp.chapterI.interfaces.Storage;
 import ru.revdaalex.lsp.chapterI.storage.Warehouse;
+import ru.revdaalex.lsp.chapterII.food.ReproductFood;
+import ru.revdaalex.lsp.chapterII.interfaces.Rstorage;
 
 import java.util.ArrayList;
 
@@ -14,17 +16,27 @@ public class NewWarehouse extends Warehouse implements Storage {
 
     private final ArrayList<Food> newWarehouse = new ArrayList<>();
 
-
+    /**
+     * add method from parent class.
+     * @param food
+     */
     public void add(Food food) {
         this.newWarehouse.add(food);
     }
 
-
+    /**
+     * boolean sortQuality method from parent class.
+     * @param food
+     * @return
+     */
     public boolean sortQuality(Food food) {
         return super.sortQuality(food);
     }
 
-
+    /**
+     * get method from parent class.
+     * @return
+     */
     public ArrayList<Food> getFood() {
         return this.newWarehouse;
     }
