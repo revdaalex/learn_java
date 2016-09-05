c883ec8b2be429221df73722aee4e47c089161e7
revdaalex <revdaalex@gmail.com>
2016-09-05 16:40:06 +0500

    modified ControllTest.java

--- LSP/ChapterII/src/test/java/ru/revdaalex/lsp/chapterII/ControllTest.java
+++ LSP/ChapterII/src/test/java/ru/revdaalex/lsp/chapterII/ControllTest.java
@@ -2,14 +2,14 @@ package ru.revdaalex.lsp.chapterII;
 
 import org.junit.Assert;
 import org.junit.Test;
-import ru.revdaalex.lsp.chapterI.control.ControllQuality;
-import ru.revdaalex.lsp.chapterI.food.Apple;
 import ru.revdaalex.lsp.chapterI.interfaces.Storage;
 import ru.revdaalex.lsp.chapterII.food.Meat;
+import ru.revdaalex.lsp.chapterII.food.Sausages;
+import ru.revdaalex.lsp.chapterII.interfaces.Rstorage;
+import ru.revdaalex.lsp.chapterII.rcontroll.RCotrollQuality;
 import ru.revdaalex.lsp.chapterII.storage.ColdWarehouse;
 import ru.revdaalex.lsp.chapterII.storage.NewWarehouse;
 
-import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
 /**
@@ -18,19 +18,17 @@ import java.util.GregorianCalendar;
  */
 public class ControllTest {
 
-    ArrayList<Storage> list = new ArrayList<Storage>();
-
     /**
      * Test sort food in new warehouse.
      */
     @Test
     public void testSortNewWarehouse(){
         Storage newWarehouse = new NewWarehouse();
-        list.add(newWarehouse);
-        ControllQuality controll = new ControllQuality(list);
-        Apple apple = new Apple("Яблоко", new GregorianCalendar(2016, 10, 21), new GregorianCalendar(2016, 8, 3), 50);
-        controll.sort(apple);
-        Assert.assertTrue(newWarehouse.getFood().contains(apple));
+        RCotrollQuality controll = new RCotrollQuality();
+        controll.add(newWarehouse);
+        Sausages sausages = new Sausages("Сосиски", new GregorianCalendar(2016, 12, 21), new GregorianCalendar(2016, 8, 3), 50);
+        controll.rSort(sausages);
+        Assert.assertTrue(newWarehouse.getFood().contains(sausages));
     }
 
     /**
@@ -38,11 +36,11 @@ public class ControllTest {
      */
     @Test
     public void testSortColdWarehouse(){
-        Storage coldWarehouse = new ColdWarehouse();
-        list.add(coldWarehouse);
-        ControllQuality controll = new ControllQuality(list);
+        Rstorage coldWarehouse = new ColdWarehouse();
+        RCotrollQuality controll = new RCotrollQuality();
+        controll.add(coldWarehouse);
         Meat meat = new Meat("Мясо", new GregorianCalendar(2016, 8, 15), new GregorianCalendar(2016, 8, 9), 300);
-        controll.sort(meat);
+        controll.rSort(meat);
         Assert.assertTrue(coldWarehouse.getFood().contains(meat));
     }
 }
