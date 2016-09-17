77f87cbd5d6af54640349604e59379120fe1083a
revdaalex <revdaalex@gmail.com>
2016-09-17 14:08:09 +0500

    add ShowMenu.java

---
+++ ISP/Menu/src/main/java/ru/revdaalex/menu/interfaces/ShowMenu.java
@@ -0,0 +1,12 @@
+package ru.revdaalex.menu.interfaces;
+
+/**
+ * Show menu interface.
+ * Created by revdaalex on 14.09.2016.
+ */
+public interface ShowMenu {
+    /**
+     * Show method.
+     */
+    void show();
+}
