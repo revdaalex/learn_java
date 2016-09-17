861efa7a9e6f0e257b3b85c58f873aa74d0d2b58
revdaalex <revdaalex@gmail.com>
2016-09-17 14:07:20 +0500

    add Output.java

---
+++ ISP/Menu/src/main/java/ru/revdaalex/menu/interfaces/Output.java
@@ -0,0 +1,13 @@
+package ru.revdaalex.menu.interfaces;
+
+/**
+ * Output interface.
+ * Created by revdaalex on 14.09.2016.
+ */
+public interface Output {
+    /**
+     * Print method.
+     * @param value
+     */
+    void println(Object value);
+}
