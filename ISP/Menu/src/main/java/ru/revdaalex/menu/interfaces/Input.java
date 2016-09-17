ca75f43fc838210c71ab3ad9f229a619f6c400c4
revdaalex <revdaalex@gmail.com>
2016-09-17 14:07:00 +0500

    add Input.java

---
+++ ISP/Menu/src/main/java/ru/revdaalex/menu/interfaces/Input.java
@@ -0,0 +1,13 @@
+package ru.revdaalex.menu.interfaces;
+
+/**
+ * Input interface.
+ * Created by revdaalex on 15.09.2016.
+ */
+public interface Input {
+    /**
+     * Read method.
+     * @return
+     */
+    String read();
+}
