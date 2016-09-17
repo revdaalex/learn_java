f3d10f395df73da207a68d8e2c57ac8893b3a7c4
revdaalex <revdaalex@gmail.com>
2016-09-17 14:06:00 +0500

    add Action.java

---
+++ ISP/Menu/src/main/java/ru/revdaalex/menu/interfaces/Action.java
@@ -0,0 +1,12 @@
+package ru.revdaalex.menu.interfaces;
+
+/**
+ * Action interface.
+ * Created by revdaalex on 15.09.2016.
+ */
+public interface Action {
+    /**
+     * User choice method.
+     */
+    void userChoice();
+}
