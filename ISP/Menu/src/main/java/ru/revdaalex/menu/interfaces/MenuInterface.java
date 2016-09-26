a5f7a6ef63302d5a8fb9353afcc071ce7bdd2913
revdaalex <revdaalex@gmail.com>
2016-09-26 16:28:40 +0500

    add new file

---
+++ ISP/Menu/src/main/java/ru/revdaalex/menu/interfaces/MenuInterface.java
@@ -0,0 +1,16 @@
+package ru.revdaalex.menu.interfaces;
+
+/**
+ * Menu Interface.
+ * Created by revdaalex on 26.09.2016.
+ */
+public interface MenuInterface {
+
+    void getName();
+
+    void getParent();
+
+    void getMenuLevel();
+
+    void setSelected();
+}
