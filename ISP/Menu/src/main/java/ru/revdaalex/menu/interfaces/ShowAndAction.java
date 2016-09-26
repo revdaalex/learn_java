dbad71652d408f07f902aa0802e80fa4d5d3f17b
revdaalex <revdaalex@gmail.com>
2016-09-26 16:23:07 +0500

    delete

--- ISP/Menu/src/main/java/ru/revdaalex/menu/interfaces/ShowAndAction.java
+++
@@ -1,8 +0,0 @@
-package ru.revdaalex.menu.interfaces;
-
-/**
- * Show and action interface.
- * Created by revdaalex on 15.09.2016.
- */
-public interface ShowAndAction extends Action, ShowMenu {
-}
